eb62d335d6783994d09217eae190eda9b5713f56
Nadeem Khan <imkhan.1910@gmail.com>
2025-01-31 19:53:42 +0530

    path module code added

---
+++ pathModule.js
@@ -0,0 +1,8 @@
+const path = require('path')
+
+console.log(path.extname('D:\js\node\textFile.txt'))
+
+console.log(path.basename('D:\\js\\node\\textFile.txt'))
+
+console.log(__filename)
+console.log(__dirname)
